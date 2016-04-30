87f69337c127ef51bbbfe58db0b808bfd5ab28b8
Mario Finelli <mario@finel.li>
2016-04-30 08:02:34 -0400

    Add function to perform uppercase redirect

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = function *(next) {
+  if (/[A-Z]/.test(this.request.url)) {
+    this.status = 301;
+    this.redirect(this.url.toLowerCase());
+  } else {
+    yield next;
+  }
+};
